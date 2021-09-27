6c802cd2886809c4cdd9b1ed7f95411a4474d2f8
Khoi Dao <khoidao55@gmail.com>
2021-09-26 22:42:19 -0700

    getting data from api

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const WebSocket = require('ws');
+const express = require('express');
+const PORT = 3001;
+const { API_KEY } = require('../finnhub-api.js');
+
+const app = express();
+
+// app.use(bodyParser.json({limit: "50mb"}));
+// app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+const socket = new WebSocket(`wss://ws.finnhub.io?token=${API_KEY}`);
+
+// Connection opened -> Subscribe
+socket.addEventListener('open', function (event) {
+    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
+    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
+    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'IC MARKETS:1'}))
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
+
+// Unsubscribe
+ var unsubscribe = function(symbol) {
+    socket.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
+}
+
+app.listen(PORT, () => {
+  console.log("Listening on 3001");
+})
